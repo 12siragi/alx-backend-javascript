03f052c803f41ac7eab4e46a4f0d05391a103d27
12siragi <siragisaidi218@gmail.com>
2024-08-05 21:40:32 +0000

    js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,8 @@
 export default function createIteratorObject(report) {
-  // Get the values (employee lists) from the allEmployees object
-  const employeeLists = Object.values(report.allEmployees);
-  
-  // Flatten the list of employee arrays into a single array
-  const allEmployees = employeeLists.flat();
-  
-  // Return an iterator over the flattened array
-  return allEmployees[Symbol.iterator]();
+  const reportWithIterator = [];
+
+  for (const value of Object.values(report.allEmployees)) {
+    reportWithIterator.push(...value);
+  }
+  return reportWithIterator;
 }
