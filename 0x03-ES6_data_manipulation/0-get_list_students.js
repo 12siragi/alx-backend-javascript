1a12e5d2febda87d680b6e79cf711f736e970abc
12siragi <siragisaidi218@gmail.com>
2024-08-13 04:32:40 +0000

    man

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-// 0-get_list_students.js
-
 export default function getListStudents() {
-  return [
+  const myArr = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  return myArr;
 }
